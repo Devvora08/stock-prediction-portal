220c7cb83e4cea880cb406f9f136f8e91bbe2302
devvora08 <devvora08@gmail.com>
2025-08-06 10:52:00 +0530

    login

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Button from './Button'
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../AuthProvider'
 
 const Header = () => {
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+    setIsLoggedIn(false)
+    navigate('/login')
+  }
+
   return (
     <>
-        <nav className='navbar container pt-3 pb-3 align-items-start'>
-            <Link className='navbar-brand text-light' to={'/'}>Stock Prediction Portal</Link>
-            <div>
-                <Button url='/login' text="Login" class={'btn-outline-info'}/>
-                &nbsp;
-                <Button url='/register' text={'Register'} class="btn-info"/>
-            </div>
-        </nav>
+      <nav className='navbar container pt-3 pb-3 align-items-start'>
+        <Link className='navbar-brand text-light' to={'/'}>Stock Prediction Portal</Link>
+        <div>
+          {
+            isLoggedIn ? (<button className='btn btn-danger' onClick={handleLogout}>Logout</button>)
+              : (
+                <>
+                  <Button url='/login' text="Login" class={'btn-outline-info'} />
+                  &nbsp;
+                  <Button url='/register' text={'Register'} class="btn-info" />
+                </>
+              )
+          }
+
+        </div>
+      </nav>
     </>
   )
 }
--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useContext, useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+import { AuthContext } from '../AuthProvider'
 
 const Login = () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [errors, setErrors] = useState('')
+  const {isLoggedIn, setIsLoggedIn} = useContext(AuthContext)
+  
+  const navigate = useNavigate()
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true)
+
+    const userData = {
+      username, password
+    }
+    //console.log(userData)
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/v1/token/', userData)
+      // console.log(response.data)
+      localStorage.setItem('accessToken', response.data.access)
+      localStorage.setItem('refreshToken', response.data.refresh)
+      console.log('Login Successful')
+      setIsLoggedIn(true)
+      navigate('/')
+    } catch (error) {
+      console.log(', invalid credentials', error)
+      setErrors(error)
+    } finally {
+      setLoading(false)
+      setErrors('')
+      setUsername('')
+      setPassword('')
+    }
+  }
+
   return (
-    <div className='text-light container'>Login</div>
+    <>
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-md-6 bg-light-dark p-5 rounded'>
+            <h3 className='text-light text-center mb-5'>Login to Portal</h3>
+            <form onSubmit={handleLogin}>
+              <div className='mb-3'>
+                <input value={username} onChange={e => setUsername(e.target.value)} type="text" className='form-control' placeholder='Enter username' />
+              </div>
+              <div className='mb-4'>
+                <input value={password} onChange={e => setPassword(e.target.value)} type="password" className='form-control' placeholder='Enter password' />
+              </div>
+              <button disabled={loading} type='submit' className='btn btn-info mb-3 d-block mx-auto'>Login</button>
+              {errors && <div className='text-danger'>{errors}</div>}
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
